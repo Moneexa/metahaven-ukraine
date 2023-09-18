be12cd85bc53c44519ebff63de53570cca535c4c
Moneeza Syed <14besemsyed@seecs.edu.pk>
2023-09-18 22:25:52 +0200

    added language

---
+++ front-end/src/languages/Translations.ts
@@ -0,0 +1,108 @@
+export default
+    {
+        "DONATE": {
+            "ES": "Donar",
+            "FR": "FAIRE UN DON",
+            "UA": "ПОЖЕРТВУЙТЕ"
+        },
+        "ACCESS THE LANDMARK'S HUB": {
+            "ES": "ACCEDER AL HUB DE Hitos",
+            "FR": "ACCÉDER AU HUB DES REPÈRES",
+            "UA": "ДОСТУП ДО ОРІЄНТІВ",
+        },
+        "ACCESS THE HUB": {
+            "ES": "ACCEDER AL CENTRO",
+            "FR": "ACCÉDER AU HUB",
+            "UA": "ДОСТУП ДО ХАБУ"
+        },
+        "DONATE NOW": {
+            "ES": "DONE AHORA",
+            "FR": "FAIRE UN DON MAINTENANT",
+            "UA": "ПОЖЕРТВУЙ ЗАРАЗ"
+        },
+        "UKRAINIAN LANDMARKS FOR POSTERITY": {
+            "ES": "HITO UCRANIANO PARA LA POSTERIDAD",
+            "FR": "DES REPÈRES UKRAINIENS POUR LA POSTÉRITÉ",
+            "UA": "УКРАЇНСЬКІ ПАМ'ЯТКИ ДЛЯ НАЩАДОК",
+        },
+        "METAHAVEN UKRAINE": {
+            "ES": "METAHAVEN UCRANIA",
+            "FR": "METAHAVEN UKRAINE",
+            "UA": "МЕТАХАВЕН УКРАЇНА",
+        },
+        "DIGITAL CONSERVATION AND AWARENESS EFFORTS": {
+            "ES": "CONSERVACIÓN DIGITALY ESFUERZOS DE CONCIENCIACIÓN",
+            "FR": "CONSERVATION NUMÉRIQUE ET EFFORTS DE SENSIBILISATION",
+            "UA": "ЦИФРОВЕ ЗБЕРЕЖЕННЯ І ОБІЗНАНОСТІ",
+        },
+        "AVAILABLE LANDMARKS": {
+            "ES": "LUGARES DISPONIBLES",
+            "FR": "REPÈRES DISPONIBLES",
+            "UA": "НАЯВНІ ОРІЄНТИ"
+        },
+        "KHARKIV GOVERNMENT BUILDING": {
+            "ES": "GOBIERNO DE JÁRKIV EDIFICIO",
+            "FR": "GOUVERNEMENT DE KHARKIV BÂTIMENT",
+            "UA": "ХАРКІВСЬКИЙ УРЯД БУДІВЛЯ"
+        },
+        "COMING SOON": {
+            "ES": "MUY PRONTO",
+            "FR": "À VENIR",
+            "UA": "НЕЗАБАРОМ"
+        },
+        "XXX I STREET": {
+            "ES": "CALLE XXX I,",
+            "FR": "XXX JE RUE,",
+            "UA": "ХХХ І ВУЛ."
+        },
+        "DISTRICT OF COLUMBIA 200006": {
+            "ES": "DISTRICT OF COLUMBIA 20006",
+            "FR": "DISTRICT DE COLUMBIA 20006",
+            "UA": "ОКРУГ КОЛУМБІЯ 20006",
+        },
+        "FOLLOW US HERE": {
+            "ES": "SÍGUENOS AQUÍ",
+            "FR": "SUIVEZ-NOUS ICI",
+            "UA": "СЛЕДУЙТЕ ЗА НАМИ ТУТ",
+        },
+        "METAVERSE": {
+            "ES": "METAVERSO",
+            "FR": "MÉTAVERS",
+            "UA": "МЕТАВСЕСВІТ",
+        },
+        "DONATE TO SUPPORT THE RECONSTRUCTION EFFORTS": {
+            "ES": "DONAR PARA APOYAR LOS ESFUERZOS DE RECONSTRUCCIÓN",
+            "FR": "FAIRE UN DON POUR SOUTENIR LES EFFORTS DE RECONSTRUCTION",
+            "UA": "ПОЖЕРТВУЙТЕ НА ПІДТРИМКУ РЕКОНСТРУКЦІЇ",
+        },
+        "I WOULD LIKE TO": {
+            "ES": "ME GUSTARÍA DAR",
+            "FR": "JE VOUDRAIS DONNER",
+            "UA": "Я ХОЧУ ДАТИ",
+        },
+        "RAISED": {
+            "ES": "AUMENTÓ",
+            "FR": "SOULEVÉ",
+            "UA": "ПІДНЯТА"
+        },
+        "DONATIONS": {
+            "ES": "AUMENTÓ",
+            "FR": "SOULEVÉ",
+            "UA": "ПІДНЯТА"
+        },
+        "GOAL": {
+            "ES": "META",
+            "FR": "BUT",
+            "UA": "МЕТА"
+        },
+        "DONOR WALL": {
+            "ES": "MURO ​​DE DONANTESMURO",
+            "FR": "MUR DES DONATEURS",
+            "UA": "ДОНОРСЬКА СТІНА"
+        },
+        "ABOUT PAGE": {
+            "ES": "ACERCA DE LA PÁGINA",
+            'FR': "À PROPOS DE LA PAGE",
+            "UA": "ПРО СТОРІНКУ"
+        }
+    }
